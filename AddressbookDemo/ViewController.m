ad683d380246be16da93deff0ce0037b6f67fe87
Donal <donal.tong@gmail.com>
2014-01-08 22:21:31 +0800

    Initial Commit

---
+++ AddressbookDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  AddressbookDemo
+//
+//  Created by Donal on 14-1-8.
+//  Copyright (c) 2014年 vikaa. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
